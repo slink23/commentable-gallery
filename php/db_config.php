9f78c4a9b3486768e62037bb0c3e8045845a1aa7
slink23 <slink@gchrl.org>
2015-06-17 10:39:33 -0400

    Create db_config.php

---
+++ php/db_config.php
@@ -0,0 +1,7 @@
+<?php
+// Define your MySQL connection here.
+define("DB_NAME", "" /* Database Name Here */ );
+define("DB_HOST", "" /* SQL Host here, probably 'localhost' */ );
+define("DB_USER", "" /* SQL Username here */ );
+define("DB_PASS", "" /* Database password here */ );
+?>
